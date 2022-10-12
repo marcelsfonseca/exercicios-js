ce9b68bf7803a18d2d2774233bd8e01b0e575456
marcelsfonseca <marcelsfonseca@gmail.com>
2022-10-12 14:51:39 -0300

    exercicio-Mat

---
+++ operacaoMat.js
@@ -0,0 +1,5 @@
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+
+console.log("A soma é: ", soma, "A multiplecação é: ", multiplicacao, "A equação é:", operacao)
