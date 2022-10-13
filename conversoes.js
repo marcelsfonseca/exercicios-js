6fd09dee624f09b649ec2bc43ac559ed765fe99b
marcelsfonseca <marcelsfonseca@gmail.com>
2022-10-12 22:30:20 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+const numero = 35;
+const numeroString = "35";
+
+//compara o tipo e o valor
+console.log(numero === numeroString);
+//compara o valor e faz um conversão de tipo de forma implicita
+console.log(numero == numeroString)
+
+//Funçoes para conversão de tipos.
+// Number() eo String()
+
+console.log(numero + Number(numeroString))
